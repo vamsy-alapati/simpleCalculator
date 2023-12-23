97feedae7542aaf873196a6e9d63af36d89c6813
VAMSY ALAPATI <alapati.vamsy1996@gmail.com>
2023-12-23 13:38:48 -0500

    calculator code complete

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const display = document.querySelector(".display input");
-const buttons = document.querySelectorAll(".calculator div input");
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
 const operators = ["%", "/", "*", "-", "+", "="];
 let output="";
 
@@ -8,10 +8,10 @@ let output="";
 
 //Define function to calculate based on button clicked
 const calculate = (val) => {
-  // console.log(val);
+  console.log(val);
   if (val === "=" && val !== "") {
     //if the operator is % then replace with '/100' before evaluating
-    output = eval(output.replace("%", "/100"));
+    output = eval(output);
   }
   else if (val === "AC") {
     //if the operator is "AC" then clear the screen
@@ -24,11 +24,12 @@ const calculate = (val) => {
   else {
     //if the output is empty and button is a special character then just return
     if (output === "" && operators.includes(val)) return;
+    output += val;
   }
   display.value = output;
 }
 
 // add event listener to buttons, call calculate() on click of "="
 buttons.forEach((button) => {
-  button.addEventListener("click", (e) => calculate(e.target.value));
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
 });
